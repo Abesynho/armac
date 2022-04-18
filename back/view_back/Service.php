356a7085dfde5519b8230aaf3067f541437806e9
mouhamed <mouhamed.abbassi@esprit.tn>
2022-04-18 05:34:31 +0100

    Crud service && integration template front&back

--- back/view_back/Service.php
+++ back/view_back/Service.php
@@ -1,63 +1,66 @@
-
 <?php include "nav-bar.php" ?>
+<?php
+include('../controller_back/service_C.php');
+$service_C = new service_C();
+$response = $service_C->afficherService($service_C->conn);
 
+?>
     <div class="row tm-content-row tm-mt-big">
-        <div class="tm-col tm-col-big"> </div>
-        <div class="tm-col tm-col-big">
-            <div class="bg-white tm-block">
-                <div class="row">
-                    <div class="col-12">
-
-                        <h2 class="tm-block-title">Edit Service</h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <form action="" class="tm-signup-form">
-                            <div class="form-group">
-                                <label for="name">Nom</label>
-                                <input placeholder="beauty name" id="name" name="name" type="text" class="form-control validate">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="name"> Type</label>
-                                <input placeholder="type name" id="type" name="type" type="text" class="form-control validate">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="name"> prix</label>
-                                <input placeholder="0dt" id="prix" name="prix" type="number" class="form-control validate">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="name"> Description</label>
-                                <input placeholder="Write message" id="description" name="description" type="text" class="form-control validate">
-                            </div>
-
-
-                            <div class="row">
-                                <div class="col-12 col-sm-4">
-                                    <button type="submit" class="btn btn-primary">Add
-                                    </button>
-                                </div>
-                                <div class="col-12 col-sm-8 tm-btn-right">
-                                    <button type="submit" class="btn btn-danger">Delete
-                                    </button>
-                                </div>
-
-                                <div class="col-12 col-sm-7  tm-btn-right">
-                                    <button type="submit" class="btn btn-dark">Update
-                                    </button>
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
+    <div class="col-xl-8 col-lg-12 tm-md-12 tm-sm-12 tm-col">
+    <div class="bg-white tm-block h-100">
+        <div class="row">
+            <div class="col-md-8 col-sm-12">
+                <h2 class="tm-block-title d-inline-block">Service</h2>
+            </div>
+            <div class="col-md-4 col-sm-12 text-right">
+                <a href="addService.php" class="btn btn-small btn-primary">Add New Service</a>
             </div>
         </div>
-        <div class="tm-col tm-col-small"> </div>
 
+        <div class="table-responsive">
+            <?php
+            while ($service = $response->fetch()) {
+                ?>
+                <table class="table table-hover table-striped tm-table-striped-even mt-3">
+                    <thead>
+                    <tr class="tm-bg-gray">
+
+                        <th> id</th>
+                        <th> Type</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td> <?php echo $service['id']; ?></td>
+                        <td> <?php echo $service['type']; ?></td>
+                        <td><?php echo $service['nom']; ?></td>
+                        <td><?php echo $service['prix']; ?></td>
+                        <td><?php echo $service['description']; ?></td>
+                        <td> <form action="../core/deleteService.php" method="POST" enctype="multipart/form-data">
+                                <button type="submit" name="id" value="<?php echo $service['id']; ?>"
+                                        class="btn btn-danger">Delete
+                                </button>
+                            </form></td><td> <form action="updateService.php" method="POST" enctype="multipart/form-data">
+                                <button type="submit" name="id" value="<?php echo $service['id']; ?>"
+                                        class="btn btn-danger">Update
+                                </button>
+                            </form></td>
+                    </tr>
+                    </tbody>
+                </table>
+            <?php } ?>
+        </div>
+
+        <div class="tm-table-mt tm-table-actions-row">
+            <div class="tm-table-actions-col-left">
+
+
+            </div>
+        </div>
     </div>
 
 <?php include "Footer.php" ?>
